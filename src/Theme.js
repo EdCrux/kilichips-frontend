db7bad394755709e94eef7fac2392ec6c2676916
Eduardo <jecruxiz@gmail.com>
2020-08-24 17:33:44 -0500

    :lipstick: Update UI theme for responsive font's

--- src/Theme.js
+++ src/Theme.js
@@ -2,14 +2,19 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
   typography: {
-    fontFamily: 'Montserrat'
+    fontFamily: 'Montserrat',
+    h1: {
+      fontFamily: 'Montserrat'
+    }
   },
   palette: {
     primary: {
       main: '#fafafa',
+      dark: '#8bd564'
     },
     secondary: {
       main: '#ff5000',
+      dark: '#b23800'
     },
   },
 });
